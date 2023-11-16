7a04fb4d7acb6e0ea12498d1c9501176d74f0c11
Patrick Rutkowski <patrick.rutkowski@banter.agency>
2023-11-16 16:17:44 +1100

    connect form to firebase, pre image upload

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,45 +1,99 @@
-import React, { useState } from "react";
-import firebase from "firebase/compat/app";
-import 'firebase/firestore';
+"use client"
 
+import React, { useState } from "react";
+import { collection, addDoc } from "firebase/firestore"; // Importing new Firebase functions
+import { db } from "../firebase"; // Ensure this is set up for Firebase 10.6.0
+
+// Main form that submits to Firebase Firestore
 const Form = () => {
+
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        emailAddress: '',
+        phoneNumber: '',
+        receipt: null,
+        marketing: false,
+        terms: false
+    });
+
+    const handleChange = (e) => {
+        if (e.target.type === 'file') {
+            setFormData({
+                ...formData, [e.target.name]: e.target.files[0]
+            });
+        }
+        else if (e.target.type === 'checkbox') {
+            setFormData({
+                ...formData, [e.target.name]: e.target.checked
+            });
+        }
+        else {
+            setFormData({
+                ...formData, [e.target.name]: e.target.value
+            });
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        try {
+            // Using new Firestore API to add a document
+            await addDoc(collection(db, "forms"), formData);
+    
+            setFormData({
+                firstName: '',
+                lastName: '',
+                emailAddress: '',
+                phoneNumber: '',
+                receipt: null,
+                marketing: false,
+                terms: false
+            });
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
+    };
+
+
     return (
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <fieldset className="flex flex-col items-center border-2 ">
              <div className="flex flex-row border-2">
                 <div>
-                    <label htmlFor="first-name">First Name</label>
-                    <input className="flex flex-col border-2 border-slate-800" type="text" name="First name" required/>
+                    <label htmlFor="firstName">First Name</label>
+                    <input className="flex flex-col border-2 border-slate-800" type="text" name="firstName" onChange={handleChange} required/>
                 </div>
                 <div>
-                    <label htmlFor="last-name">Last Name</label>
-                    <input className="flex flex-col border-2 border-slate-800" type="text" name="First name" required/>
+                    <label htmlFor="lastName">Last Name</label>
+                    <input className="flex flex-col border-2 border-slate-800" type="text" name="lastName" onChange={handleChange} required/>
                 </div>
                 </div>
                 <div>
-                    <label htmlFor="email-address">Email Address</label>
-                    <input className="flex flex-col border-2 border-slate-800" type="email" name="First name" required/>
+                    <label htmlFor="emailAddress">Email Address</label>
+                    <input className="flex flex-col border-2 border-slate-800" type="email" name="emailAddress" onChange={handleChange} required/>
                 </div>
                
                 <div>
-                    <label htmlFor="phone-number">Phone Number</label>
-                    <input className="flex flex-col border-2 border-slate-800" type="tel" name="Mobile number" required/>
+                    <label htmlFor="phoneNumber">Phone Number</label>
+                    <input className="flex flex-col border-2 border-slate-800" type="tel" name="phoneNumber" onChange={handleChange} required/>
                 </div>
                 <div>
-                    <label htmlFor="receipt">Receipt Image</label>
-                    <input className="flex flex-col border-2 border-slate-800" type="file" name="receipt" accept="image/*" required/>
+                    <label htmlFor="receipt">Receipt Upload</label>
+                    <input className="flex flex-col border-2 border-slate-800" type="file" name="receipt" accept="image/*" onChange={handleChange}/>
                 </div>
                 <div>
-                    <input type="checkbox" id="marketing" name="marketing" />
+                    <input type="checkbox" id="marketing" name="marketing" onChange={handleChange} />
                     <label htmlFor="marketing">I agree to receive marketing materials</label>
                 </div>
                 <div>
-                    <input type="checkbox" id="terms" name="terms" required />
+                    <input type="checkbox" id="terms" name="terms" onChange={handleChange} required />
                     <label htmlFor="terms">I agree to the terms and conditions</label>
                 </div>
             </fieldset>
             <div>
-                <button className="border-2 border-red-500 bg-slate-950 text-white px-4">Submit</button>
+                <button className="border-2 border-red-500 bg-slate-950 text-white px-4" type="submit">Submit</button>
             </div>
         </form>
     )
--- app/firebase.js
+++ app/firebase.js
@@ -1,31 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD6tD9Z4lvf_B3mqQ9enEi9EVQX2xWpSCU",
-  authDomain: "westinghouse-form.firebaseapp.com",
-  projectId: "westinghouse-form",
-  storageBucket: "westinghouse-form.appspot.com",
-  messagingSenderId: "412027548032",
-  appId: "1:412027548032:web:c28ee37bb8e014be94d165",
-  measurementId: "G-KJ96EKK150"
+  apiKey: "AIzaSyD0jRUqSN2vK3AjwxksvaRa9YtAyNhlAQk",
+  authDomain: "westinghouse-entry.firebaseapp.com",
+  projectId: "westinghouse-entry",
+  storageBucket: "westinghouse-entry.appspot.com",
+  messagingSenderId: "861160858844",
+  appId: "1:861160858844:web:8e5b771a27ed6b6a0c0019"
 };
 
 // Initialize Firebase
-let app;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApps()[0];
-}
+const app = initializeApp(firebaseConfig);
 
-const analytics = getAnalytics(app);
+// Initialize Firestore
 const db = getFirestore(app);
 
-export { app, analytics, db };
+export { db };
